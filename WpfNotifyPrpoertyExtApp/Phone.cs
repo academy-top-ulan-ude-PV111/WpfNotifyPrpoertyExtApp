96fd1afe2bb38b9bcbeed8b9821caf338919f49f
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-21 16:28:07 +0300

    Добавьте файлы проекта.

---
+++ WpfNotifyPrpoertyExtApp/Phone.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfNotifyPrpoertyExtApp
+{
+    internal class Phone : INotifyPropertyChanged
+    {
+        private string? model;
+        private string? company;
+
+        public string? Model
+        {
+            get => model;
+            set
+            {
+                
+                    model = value;
+                    OnPropertyChanged(nameof(Model));
+
+            }
+        }
+
+        public string? Company
+        {
+            get => company;
+            set
+            {
+                
+                    company = value;
+                    OnPropertyChanged(nameof(Company));
+
+            }
+        }
+        public Phone(string? model, string? company)
+        {
+            Model = model;
+            Company = company;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChangedEventArgs args = new PropertyChangedEventArgs(propName);
+            PropertyChanged?.Invoke(this, args);
+        }
+    }
+}
